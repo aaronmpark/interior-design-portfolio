ddfd944ef0683aad84bd09f7c188128afd98a433
Aaron Park <135677875+aaronmpark@users.noreply.github.com>
2025-10-11 13:18:52 -0400

    small touches

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,22 +1,44 @@
 <template>
     <div class="about">
         <Navbar />
+        <img :src="about_image" alt="WNB Factory" class="about-image" />
     </div> 
 </template>
 
 <script>
 import Navbar from './Navbar.vue';
-
+import about_image from '../assets/wnb_factory.jpeg';
 export default {
   name: 'DefaultAbout',  
   components: {
     Navbar
   },
   props: {
+  },
+  data() {
+    return {
+      about_image
+    };
   }
 }
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.about {
+  position: relative;
+  display: flex;
+  justify-content: center; 
+  align-items: center; 
+  width: 100vw;
+  height: 100vh;
+}
+
+
+.about-image {
+  width: 85vw;
+  height: 85vh;
+  object-fit: cover;
+  display: block;
+}
 </style>
